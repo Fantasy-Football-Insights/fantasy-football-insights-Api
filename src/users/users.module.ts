2bb87698800a93d759fdf39d7db68dc2290cd17b
nicolasjott <nicobox2002@gmail.com>
2023-10-05 16:49:50 -0400

    export UsersService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersController } from './users.controller';
+import { UsersController } from "./users.controller";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
