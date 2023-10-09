b25cf719692862463f806899004c4f7b227a39f8
Nicolas Ott <76223327+NicolasJott@users.noreply.github.com>
2023-10-09 12:58:03 -0400

    Revert "Dev (#2)"
    This reverts commit 0d37915db35f91418be2f5f8a30159bde3f528a4.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ConfigService } from "@nestjs/config";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // configures SwaggerUI
-  const config = new DocumentBuilder()
-    .setTitle("Fantasy Football Insights API")
-    .setDescription("The Fantasy Football Insights API")
-    .setVersion("1.0")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("docs", app, document);
-
-  // get PORT from config
-  const configService = app.get(ConfigService);
-  const PORT = configService.get<number>("PORT");
-
-  // start application
-  await app.listen(PORT);
-  console.log(`Application is running on: ${await app.getUrl()}/docs`);
+  await app.listen(3000);
 }
 bootstrap();
