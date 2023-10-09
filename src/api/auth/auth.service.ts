0d37915db35f91418be2f5f8a30159bde3f528a4
Nicolas Ott <76223327+NicolasJott@users.noreply.github.com>
2023-10-09 12:57:55 -0400

    Dev (#2)
    * packages
    
    * add env config
    
    * auth
    
    * implement auth
    
    * export UsersService
    
    * inject ConfigService
    
    * config swagger
    
    * cleanup tags
    
    * fix guard
    
    * config database env
    
    * password hashing
    
    * update README
    
    * remap structure
    
    * updates
    
    * update readme
    
    * comments

---
+++ src/api/auth/auth.service.ts
@@ -0,0 +1,58 @@
+import {
+  Injectable,
+  UnauthorizedException,
+  NotFoundException,
+} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { UsersService } from "src/api/users/users.service";
+import { JwtService } from "@nestjs/jwt";
+import { comparePassword } from "src/core/security";
+import { User } from "src/entities/users/user.entity";
+import { CreateUserSchema } from "src/schemas/users/create-user.schema";
+import { hashPassword } from "src/core/security";
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async register(createUserIn: CreateUserSchema): Promise<User> {
+    const { email, firstName, lastName, password } = createUserIn;
+    const hashedPassword = hashPassword(password);
+
+    const user = await this.usersRepository.save({
+      email,
+      firstName,
+      lastName,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+
+  async signIn(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(email);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    if (!comparePassword(password, user.password)) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.id, email: user.email };
+
+    return { access_token: await this.jwtService.signAsync(payload) };
+  }
+
+  async doesUserExist(email: string): Promise<boolean> {
+    const user = await this.usersService.findOne(email);
+    if (user) {
+      return true;
+    }
+    return false;
+  }
+}
