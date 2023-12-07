d9607f9e2ebd89f6a70c1611c44c7e86a9eba758
nicolasjott <nicobox2002@gmail.com>
2023-12-07 13:05:32 -0500

    Refactor roster controller and service

--- src/schemas/roster/roster.schemas.ts
+++ src/schemas/roster/roster.schemas.ts
@@ -6,6 +6,30 @@ export class CreateRosterResponse {
   @ApiProperty({ example: 1, description: "ID" })
   @IsNumber()
   id: number;
+
+  @ApiProperty({ example: 1, description: "Owner ID" })
+  @IsNumber()
+  ownerId: number;
+
+  @ApiProperty({ example: 1, description: "Draft Position" })
+  @IsNumber()
+  draftPosition: number;
+
+  @ApiProperty({ example: "String", description: "League Name" })
+  @IsString()
+  leagueName: string;
+
+  @ApiProperty({ example: "String", description: "Team Name" })
+  @IsString()
+  teamName: string;
+
+  @ApiProperty({ example: 10, description: "League Size" })
+  @IsNumber()
+  leagueSize: number;
+
+  @ApiProperty({ example: 10, description: "League Size" })
+  @IsNumber()
+  players: JSON;
 }
 
 export class CreateRosterRequest {
