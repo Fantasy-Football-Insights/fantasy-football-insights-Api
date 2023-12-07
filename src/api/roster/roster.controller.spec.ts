7fec4573d5669e9261de503171aa9c6b2269ee73
Scarletbobcat <tien.hoan314@yahoo.com>
2023-12-07 14:05:09 -0500

    roster findall function test written

---
+++ src/api/roster/roster.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { RosterController } from './roster.controller';
+import { RosterService } from './roster.service';
+
+describe('AuthController', () => {
+  let controller: RosterController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RosterController],
+      providers: [{
+        provide: RosterService,
+        useValue: {
+            findAll: jest.fn((x) => x)
+        }
+      }]
+    }).compile();
+
+    controller = module.get<RosterController>(RosterController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('FindAll', () => {
+    it('should return array of players', async () => {
+        await controller.findAll();
+    })
+  })
+});
