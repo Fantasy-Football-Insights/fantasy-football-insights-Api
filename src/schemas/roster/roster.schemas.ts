ce4a1a9a552f6e1b3f0af8fe1e047ca2ab0aba0e
nicolasjott <nicobox2002@gmail.com>
2023-12-07 12:59:54 -0500

    Refactor roster entity and schema

--- src/schemas/roster/roster.schemas.ts
+++ src/schemas/roster/roster.schemas.ts
@@ -1,5 +1,5 @@
-import { IsNumber} from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
 
 // schema that is used when a user signs in
 export class CreateRosterResponse {
@@ -12,10 +12,22 @@ export class CreateRosterRequest {
   @ApiProperty({ example: 1, description: "Draft Position" })
   @IsNumber()
   draftPosition: number;
+
+  @ApiProperty({ example: "String", description: "League Name" })
+  @IsString()
+  leagueName: string;
+
+  @ApiProperty({ example: "String", description: "Team Name" })
+  @IsString()
+  teamName: string;
+
+  @ApiProperty({ example: 10, description: "League Size" })
+  @IsNumber()
+  leagueSize: number;
 }
 
 export class DeleteRosterRequest {
-  @ApiProperty({ example: 1, description: "ID"})
+  @ApiProperty({ example: 1, description: "ID" })
   @IsNumber()
   id: number;
 }
